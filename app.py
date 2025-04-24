16c34b53ca01d6dbc107f8b2b056f3b83dad9e6a
Chitransh <chitranshgaur@outlook.com>
2025-04-24 11:12:37 +0530

    news compartments designed for thehindu

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('layout.html',title='Home')
+    return render_template('layout.html',title='TheNewsGround')
+
+@app.route("/thehindu")
+def thehindu():
+    return render_template('TheHindu.html', title='TheHindu')
 
 if __name__=="__main__":
     app.run(debug=True)
