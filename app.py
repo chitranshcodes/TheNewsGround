fdea1646e6b250b05fed515c6af1da0ffcc6bb03
Chitransh <chitranshgaur@outlook.com>
2025-04-24 14:14:59 +0530

    wtforms made and form templates also made

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.secret_key='njifnvdfsdcdcrvsdccdc'
+
 
 @app.route("/")
 def home():
@@ -9,6 +11,20 @@ def home():
 @app.route("/thehindu")
 def thehindu():
     return render_template('TheHindu.html', title='TheHindu')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+    return render_template('login.html', form=form)
 
 if __name__=="__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, optional
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional
 class RegistrationForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired(), Length(min=6)])
     email=StringField('Email', validators=[DataRequired(), Email()])
-    mob_num=IntegerField('mobile number', validators=[optional(), Length(min=10,max=10)])
+    mob_num=IntegerField('mobile number', validators=[Optional()])
     password=PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password=PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='passwords must match')])
     submit=SubmitField('Sign-Up')
