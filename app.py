bc4e6e7861eaa0cf1eaefd3ab080212eadb6c2b8
Chitransh <chitranshgaur@outlook.com>
2025-04-26 23:47:53 +0530

    username already exists, choose different mail and username

--- app.py
+++ app.py
@@ -1,24 +1,50 @@
 from flask import Flask, render_template, redirect, url_for, flash
-from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional, ValidationError
 
 app = Flask(__name__)
 app.secret_key='njifnvdfsdcdcrvsdccdc'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db=SQLAlchemy(app)
 
+#DB models
 class User(db.Model):
     id=db.Column(db.Integer(), primary_key=True)
-    username=db.Column(db.String(20), nullable=False, unique=True)
+    username=db.Column(db.String(20), nullable=False)
     email=db.Column(db.String(100), nullable=False)
     password=db.Column(db.String(60), nullable=False)
     number=db.Column(db.String(10))
 
     def __repr__(self):
         return f"User({self.username},{self.email},{self.id})"
+    
+#FORMS
+class RegistrationForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired(), Length(min=6)])
+    email=StringField('Email', validators=[DataRequired(), Email()])
+    mob_num=IntegerField('mobile number', validators=[Optional()])
+    password=PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password=PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='passwords must match')])
+    submit=SubmitField('Sign-Up')
 
-app.app_context().push()
-db.create_all()
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Use a different email')
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Use a different username')  
+
+
+class LoginForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired()])
+    password=PasswordField('Password', validators=[DataRequired()])
+    submit=SubmitField('Log-In')
+
+
 
 @app.route("/")
 def home():
@@ -43,7 +69,10 @@ def register():
 def login():
     form=LoginForm()
     if form.validate_on_submit():
-        flash(f"{form.username.data} Logged in!",'success')
+        user=User.query.filter_by(username=form.username.data).first()
+        if user and form.password.data==user.password:
+            
+            flash(f"{form.username.data} Logged in!",'success')
         return redirect(url_for('home'))
     return render_template('login.html', form=form)
 
