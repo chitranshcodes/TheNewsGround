198bd4a03fdf32553867a0890717211a186048f9
Chitransh <chitranshgaur@outlook.com>
2025-05-02 14:32:30 +0530

    Note db model added

--- app.py
+++ app.py
@@ -27,10 +27,15 @@ class User(db.Model, UserMixin):
     email=db.Column(db.String(100), nullable=False)
     password=db.Column(db.String(60), nullable=False)
     number=db.Column(db.String(10))
+    notes=db.relationship('Note', backref='author')
 
     def __repr__(self):
         return f"User({self.username},{self.email},{self.id})"
-    
+class Note(db.Model):
+    id= db.Column(db.Integer(), primary_key=True)
+    content=db.Column(db.Text(), nullable=False)
+    user_id=db.Column(db.Integer(), db.ForeignKey('user.id'))
+
 #FORMS
 class RegistrationForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired(), Length(min=6)])
