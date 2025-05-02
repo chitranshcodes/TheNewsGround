eec62f3ace741cc960810554bd1e129ba0c8f8ef
Chitransh <chitranshgaur@outlook.com>
2025-05-02 14:18:31 +0530

    current user loading correctly.

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ bcrypt=Bcrypt(app)
 LM=LoginManager(app)
 
 #DB models
+@LM.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class User(db.Model, UserMixin):
     id=db.Column(db.Integer(), primary_key=True)
     username=db.Column(db.String(20), nullable=False)
