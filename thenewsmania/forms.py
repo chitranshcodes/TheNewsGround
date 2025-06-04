e30f9fa5b6bf1a44dd73f5391bf05370f7c53b4f
Chitransh <chitranshgaur@outlook.com>
2025-06-04 11:48:27 +0530

    login validation messages

--- thenewsmania/forms.py
+++ thenewsmania/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional, ValidationError
 from thenewsmania.models import User
+from thenewsmania import bcrypt
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired(), Length(min=6)])
@@ -25,6 +26,18 @@ class LoginForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired()])
     password=PasswordField('Password', validators=[DataRequired()])
     submit=SubmitField('Log-In')
+
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if not user:
+            raise ValidationError('No username exists')
+    def validate_password(self,password):
+        user = User.query.filter_by(username=self.username.data).first()
+        if not user:
+            raise ValidationError("username doesn't exist")
+        if not bcrypt.check_password_hash(user.password, password.data):
+            raise ValidationError('wrong password')
+            
 
 class NoteForm(FlaskForm):
     content=StringField('content',validators=[DataRequired()])
